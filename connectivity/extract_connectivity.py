3b2f4997f88a94e398021cbf0f0c03f500c04661
John Flournoy <john_flournoy@fas.harvard.edu>
2019-10-09 15:04:46 -0400

    fixed bug for csv of parcel images

--- connectivity/extract_connectivity.py
+++ connectivity/extract_connectivity.py
@@ -19,17 +19,17 @@ from nilearn.connectome import ConnectivityMeasure
 from nilearn import plotting
 from joblib import Parallel, delayed
 
-def parallel_extract(input_file_list, label_def, labels, exclude, connectivity_obj, outpath=None, save_csv=True, num_cores=1, verbose=0):
-        print("Extracting from {} files using {} processes...".format(input_file_list.shape[0], num_cores))
-        extract_corrs = time_course_extractor(connectivity_obj, outpath=outpath)
-        extract_args_zip, rs_files_included = make_extract_arg_zips(input_file_list, label_def, exclude)
-        results = Parallel(n_jobs=num_cores, verbose=verbose)(delayed(extract_corrs)(f, l, labels=labels, save_csv=save_csv) for f, l in extract_args_zip)
-        timeseries = [rez[0] for rez in results]
-        corrmats = [rez[1] for rez in results]
-        return timeseries, corrmats, rs_files_included
+def parallel_extract(input_file_list, label_def, labels, exclude, connectivity_obj, outpath=None, save_csv=True, idcol=False, num_cores=1, verbose=0):
+    print("Extracting from {} files using {} processes...".format(input_file_list.shape[0], num_cores))
+    extract_corrs = time_course_extractor(connectivity_obj, outpath=outpath)
+    extract_args_zip, rs_files_included = make_extract_arg_zips(input_file_list, label_def, exclude, idcol)
+    results = Parallel(n_jobs=num_cores, verbose=verbose)(delayed(extract_corrs)(f, l, i, labels=labels, save_csv=save_csv) for f, l, i in extract_args_zip)
+    timeseries = [rez[0] for rez in results]
+    corrmats = [rez[1] for rez in results]
+    return timeseries, corrmats, rs_files_included
 
 def time_course_extractor(connectivity_obj, outpath=None):
-    def extract(fname, masker_fname, labels, save_csv=True):
+    def extract(fname, masker_fname, sid, labels, save_csv=True):
         if os.path.isfile(fname) and os.path.isfile(masker_fname):
             masker_obj = NiftiLabelsMasker(labels_img=masker_fname, 
                                            standardize=True,
@@ -48,15 +48,16 @@ def time_course_extractor(connectivity_obj, outpath=None):
                 if not os.path.isdir(outpath):
                     raise Exception("Cannot find output dir {}".format(outpath))
                 else:
-                    save_one(fname, time_series, cormat, labels, outpath)    
+                    save_one(fname, time_series, cormat, sid, labels, outpath)    
         else:
+            warnings.warn('Cannot find file(s) {}, {}'.format(fname,masker_fname))
             time_series = []
             cormat = []
         return time_series, cormat
     return extract
 
-def save_one(f, t, c, labels, outpath):
-    sid = re.match('.*/(sub-\d+).*_bold\.nii\.gz', f).groups()[0]
+def save_one(f, t, c, sid, labels, outpath):
+    sid = sid.astype(str)
     sid_outpath = os.path.join(outpath, sid)
     if not os.path.isdir(sid_outpath):
         try:
@@ -64,7 +65,7 @@ def save_one(f, t, c, labels, outpath):
         except:
             print("Cannot make dir {}".format(sid_outpath))
             raise
-    outfilename = re.match('.*/(sub.*)_bold\.nii\.gz', f).groups()[0]
+    outfilename = sid
     outfilename_cr = os.path.join(sid_outpath, outfilename + '_corrmat.csv')
     outfilename_ts = os.path.join(sid_outpath, outfilename + '_timeseries.csv')
 
@@ -77,24 +78,29 @@ def save_one(f, t, c, labels, outpath):
     c_df.to_csv(outfilename_cr)
     t_df.to_csv(outfilename_ts)
 
-def make_extract_arg_zips(input_filenames, label_def_filename, exclude):
-    #if the poarcel fname is a list, then assume it has a list of subject-specific 
+def make_extract_arg_zips(input_filenames, label_def_filename, exclude, idcol):
+    #if the parcel fname is a list, then assume it has a list of subject-specific 
     #label files
     input_file_list_include = input_filenames.loc[exclude == 0,'file'].values
-    
+    if idcol:
+        sids = input_filenames.loc[exclude == 0,'id'].values
+    else:
+        sids = (re.match(r'.*/(sub-\d+).*_bold\.nii\.gz', file).groups()[0] for file in input_file_list_include)
+
     label_image_file_is_nii = re.match(".*nii$", label_def_filename)
     label_image_file_is_csv = re.match(".*csv$", label_def_filename)
+
     if label_image_file_is_nii:
         print("Using {} to parcellate all images...".format(label_def_filename))
         extract_args_zip = zip(list(input_file_list_include), cycle([label_def_filename]))
     elif label_image_file_is_csv:
-        print("Using list of parcellation files from ".format(label_def_filename))
-        raise Exception("TEST THIS CODE FIRST")
+        print("Using list of parcellation files from {}".format(label_def_filename))
+    #        raise Exception("TEST THIS CODE FIRST")
         label_image_fnames = pd.read_csv(label_def_filename)
         label_image_fnames_include = label_image_fnames.loc[exclude == 0,'file'].values
-        if len(input_file_list_include) != label_image_fnames_include:
+        if len(input_file_list_include) != len(label_image_fnames_include):
             raise Exception("List of resting-state and label images do not match: {} and {}".format(len(input_file_list_include), label_image_fnames_include))
-        extract_args_zip = zip(input_filenames, label_image_fnames_include)
+        extract_args_zip = zip(input_file_list_include, label_image_fnames_include, sids)
     else:
         raise Exception("Label definition is neither .nii or .csv: {}".format(label_def_filename))
     
@@ -106,6 +112,7 @@ def main():
     t0 = time.time()
     parser = argparse.ArgumentParser(description='timeseries and connectivity matrix extraction')
     parser.add_argument('-i', type=str, help='input rs file list (csv with at least one column named \'file\')')
+    parser.add_argument('-idcol', action='store_true', help='input rs file list contains column \'id\' to use for naming output files. Note: this must be true if data does not follow BIDS naming conventions.')
     parser.add_argument('-label_img', type=str, help='label image defining parcels (nii) or list of such images (csv with at least one column named \'file\')')
     parser.add_argument('-label_names', type=str, help='filename to find label ids for use in column naming (csv with at least one column named \'label\')', default=0)
     parser.add_argument('-outname', type=str, help='name to be used for output files', default='rsfc_output')
@@ -133,6 +140,7 @@ def main():
     connectivity_kind=args.connectivity
     save_csv=not args.n
     save_mean_cor=not args.nomean and save_csv
+    idcol=args.idcol
     
     outpath = os.path.join(rsfc_derivs_dir, parcellation_name)
     if not os.path.isdir(outpath):
@@ -172,7 +180,8 @@ def main():
                                                                 exclude=exclude,
                                                                 connectivity_obj=connectivity_obj,
                                                                 outpath=outpath,
-                                                                save_csv=save_csv, 
+                                                                save_csv=save_csv,
+                                                                idcol=idcol,
                                                                 num_cores=num_cores, 
                                                                 verbose=args.verbose)
     t1 = time.time()
